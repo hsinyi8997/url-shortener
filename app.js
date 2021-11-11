cdc1668419aa5c6669f4f6cc06f83764e26b3bbf
hsinyi8997 <hsinyi8997@gmail.com>
2021-11-11 17:17:09 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-Parser')
-
+require('./config/mongoose')
 const app = express()
 const port = 3000
 const routes = require('./routes')
-
-
-mongoose.connect('mongodb://localhost/url-list')
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout:'main', extname:'.hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,14 +1,8 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const urlList = require('../url')
 const datas = require('../../test-url.json')
 const port = 3000
 const generateRandomIndex = require('../../generateRandomIndex')
-mongoose.connect('mongodb://localhost/url-list')
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
 db.once('open', () => {
   datas.forEach(data => {
