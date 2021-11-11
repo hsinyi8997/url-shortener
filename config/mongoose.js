7a5ec30b3c38bd39d436d9ed5c01128deb46f79c
hsinyi8997 <hsinyi8997@gmail.com>
2021-11-11 17:36:35 +0800

    fix: fix mongoose.js typing error

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/url-list'
 const mongoose = require('mongoose')
+mongoose.connect(MONGODB_URI)
 const db = mongoose.connection
 
 db.on('error', () => {
