e3e5cbf5524578a5e1b5d7b55420f913cd5b871e
hsinyi8997 <hsinyi8997@gmail.com>
2021-11-12 22:28:14 +0800

    fix: remove require body-parser but use express to call

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-Parser')
 require('./config/mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -8,7 +7,7 @@ const routes = require('./routes')
 
 app.engine('hbs', exphbs({ defaultLayout:'main', extname:'.hbs' }))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(routes)
 
